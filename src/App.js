2319a0042b3c60b8f2d80805b99b232d8c124e8b
Yuliia Kozik <julia.kozik@gmail.com>
2023-11-21 17:13:02 +0200

    Delete react logo file

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Hello</h1>
-      </header>
+
+      <h1>Hello</h1>
+
     </div>
   );
 }
