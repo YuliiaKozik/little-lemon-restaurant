2425718f128155c2c33e19c2bf0184393cfed598
Yuliia Kozik <julia.kozik@gmail.com>
2023-12-08 12:09:11 +0200

    Add card section tothe  Main Component

---
+++ src/components/Main/Main.js
@@ -0,0 +1,35 @@
+import Card from "../Card/Card";
+import specials from "../../specialMenuData";
+import "./Main.css";
+import Button from "../Button"
+const Main = () => {
+
+    return (
+        <>
+            <div className="cardsSection">
+                <div className="headerOfCardsSection">
+                    <h2>This week specials!</h2>
+                    <Button>Online menu</Button>
+                </div>
+
+                <div className="cards">
+                    {specials.map((spacial =>
+                        <Card
+                            key={spacial.id}
+                            img={spacial.img}
+                            dishName={spacial.dishName}
+                            price={spacial.price}
+                            description={spacial.description}
+                            delivery={spacial.delivery}
+                            icon={spacial.icon}
+
+                        />))}
+                </div>
+            </div>
+
+
+
+        </>
+    )
+}
+export default Main;
