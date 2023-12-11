124e31cf8c22bcff2bb41756efbca5152bbca40d
Yuliia Kozik <julia.kozik@gmail.com>
2023-12-11 17:26:47 +0200

    Adjust button in MainComponent and adjust Card Component

--- src/specialMenuData.js
+++ src/specialMenuData.js
@@ -7,7 +7,7 @@ const specials = [
         id: 1,
         img: saladImg,
         dishName: "Greek Salad",
-        description: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here'",
+        description: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal ",
         price: "$12.99",
         delivery: "Order a delivery",
         icon: auto
@@ -17,7 +17,7 @@ const specials = [
         id: 2,
         img: napoleonCake,
         dishName: "Napoleon Cake",
-        description: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here' ",
+        description: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has ",
         price: "$7.99",
         delivery: "Order a delivery",
         icon: auto
@@ -27,7 +27,7 @@ const specials = [
         id: 3,
         img: saladImg,
         dishName: "Salmon with salad",
-        description: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as",
+        description: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.",
         price: "$14.99",
         delivery: "Order a delivery",
         icon: auto
