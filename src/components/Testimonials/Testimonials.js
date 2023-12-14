408ec307abc48bcf9de8ae35b9d0c4a1da821daa
Yuliia Kozik <julia.kozik@gmail.com>
2023-12-14 15:58:36 +0200

    Add Testimonials Component, add changes in cardSection

--- src/components/Testimonials/Testimonials.js
+++ src/components/Testimonials/Testimonials.js
@@ -1,19 +1,25 @@
+
+import "./Testimonials.css"
 import Rating from "../Rating/Rating";
 import testimonials from "../../testimonialsData";
 
 const Testimonials = () => {
     return (
-        <>
-            {testimonials.map((testimonial) =>
-                <Rating
-                    key={testimonial.id}
-                    rating={testimonial.rating}
-                    avatarImage={testimonial.avatarImage}
-                    review={testimonial.review}
-                    name={testimonial.name}
-                />)}
+        <div className="testimonialsSection">
+            <h3>Testimonials</h3>
+            <div className="testimonialContainer">
+                {testimonials.map((testimonial) =>
+                    <Rating
+                        key={testimonial.id}
+                        rating={testimonial.rating}
+                        avatarImage={testimonial.avatarImage}
+                        review={testimonial.review}
+                        name={testimonial.name}
+                    />)}
+            </div>
 
-        </>
+
+        </div>
     )
 }
 
