2952820dec52afb60250865dd028530169f85639
Yuliia Kozik <julia.kozik@gmail.com>
2023-12-19 15:52:36 +0200

    Change Footer component & change media-screen in all components

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,69 +1,65 @@
 import "./Footer.css";
-import logoSmall from "../../assets/images/logoSmall.jpg"
+import bottomLogo from "../../assets/images/BottomLogo.png"
 
 const Footer = () => {
     return (
         <>
             <footer className="footer-container">
-                <section className="first-footer-section">
-                    <img src={logoSmall} height={100} alt="Logo with lemon"></img>
+                <section className="footer-logo">
+                    <img src={bottomLogo} alt="Logo with lemon"></img>
                 </section>
-                <section className="second-footer-section">
-                    <nav>
-                        <ul className="footer-menu">
-                            <li className="footer-item">
-                                <a href="#" className="footer-link
-                                ">Home</a>
-                            </li>
-
-                            <a href="#" className="footer-link
-                            ">About</a>
-                            <li className="footer-item">
-                            </li>
-                            <a href="#" className="footer-link
+                <div className="footer-contacts">
+                    <section className="second-footer-section">
+                        <nav>
+                            <ul className="footer-menu">
+                                <li className="footer-item">
+                                    <a href="#" className="footer-link">Home</a>
+                                </li>
+                                <li className="footer-item">
+                                    <a href="#" className="footer-link">About</a></li>
+                                <li className="footer-item">
+                                    <a href="#" className="footer-link
                             ">Menu</a>
-                            <li className="footer-item">
-                            </li>
-                            <a href="#" className="footer-link
-                            ">Reservation</a>
-                            <li className="footer-item">
-                            </li>
-                            <a href="#" className="footer-link
-                            ">Order Online</a>
-                            <li className="footer-item">
-                            </li>
-                            <a href="#" className="footer-link
-                            ">Login</a>
-                            <li className="footer-item">
-                            </li>
-                        </ul>
-                    </nav>
-                </section>
-                <section className="third-footer-section">
-                    <ul className="footer-menu">
-                        <li className="footer-item">Telephone</li>
-                        <li className="footer-item">Address</li>
-                        <li className="footer-item">E-mail</li>
-                    </ul>
-                </section>
-                <section className="forth-footer-section">
-                    <nav>
+                                </li>
+                                <li className="footer-item">
+                                    <a href="#" className="footer-link
+                            ">Reservation</a></li>
+                                <li className="footer-item">
+                                    <a href="#" className="footer-link
+                            ">Order Online</a> </li>
+                                <li className="footer-item">
+                                    <a href="#" className="footer-link
+                            ">Login</a></li>
+                            </ul>
+                        </nav>
+                    </section>
+                    <section className="third-footer-section">
                         <ul className="footer-menu">
-                            <li className="footer-item">
-                                <a href="#" className="footer-link
-                                ">Facebook</a>
-                            </li>
-                            <li className="footer-item">
-                                <a href="#" className="footer-link
-                                ">Instagram</a>
-                            </li>
-                            <li className="footer-item">
-                                <a href="#" className="footer-link
-                                ">Twitter</a>
-                            </li>
+                            <li className="footer-item">Telephone</li>
+                            <li className="footer-item">Address</li>
+                            <li className="footer-item">E-mail</li>
                         </ul>
-                    </nav>
-                </section>
+                    </section>
+                    <section className="forth-footer-section">
+                        <nav>
+                            <ul className="footer-menu">
+                                <li className="footer-item">
+                                    <a href="#" className="footer-link
+                                ">Facebook</a>
+                                </li>
+                                <li className="footer-item">
+                                    <a href="#" className="footer-link
+                                ">Instagram</a>
+                                </li>
+                                <li className="footer-item">
+                                    <a href="#" className="footer-link
+                                ">Twitter</a>
+                                </li>
+                            </ul>
+                        </nav>
+                    </section>
+                </div>
+
             </footer>
         </>
     )
