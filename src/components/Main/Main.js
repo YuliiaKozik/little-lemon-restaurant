d94d8076382867f2c1a224dee6e2859b806815ab
Yuliia Kozik <julia.kozik@gmail.com>
2024-02-15 15:53:17 +0200

    Introduce booking functionality

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -5,17 +5,18 @@ import Chicago from "../Chicago/Chicago";
 import NoPage from "../NoPage";
 import BookingPage from "../BookingPage/BookingPage";
 import { Routes, Route } from "react-router-dom";
-import { useBookings } from "../../hooks/useBookings";
+import { useAvailableTimes } from "../../hooks/useAvailableTimes";
+import { useBooking } from "../../hooks/useBooking";
 
 const Main = () => {
-    const { bookings, dispatchBooking, getAvailableTimes } = useBookings();
-    console.log("====>>>> boookings", bookings);
+    const { data, setDate } = useAvailableTimes();
+    const { dispatchBooking } = useBooking();
 
     return (
         <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/about" element={<Chicago />} />
-            <Route path="/booking-page" element={<BookingPage dispatchBooking={dispatchBooking} getAvailableTimes={getAvailableTimes} />} />
+            <Route path="/booking-page" element={<BookingPage dispatchBooking={dispatchBooking} getAvailableTimes={setDate} availableTimes={data} />} />
             <Route path="/online-menu" element={<Specials />} />
             <Route path="*" element={<NoPage />} />
         </Routes>
